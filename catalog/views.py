38651fd8c368ae8691590d4959855fed64a12193
User <annamarugina123@gmail.com>
2025-12-05 15:27:55 +0300

    Django_project

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    """Главная страница"""
+    return render(request, 'home.html')
 
 
 def contacts(request):
+    """Страница контактов с формой обратной связи"""
     if request.method == 'POST':
         name = request.POST.get('name')
         message = request.POST.get('message')
+        print('=' * 50)
+        print('Получена форма обратной связи')
+        print('=' * 50)
+        print(f'Имя: {name}')
+        print(f'Сообщение: {message}')
+        print('=' * 50)
         return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'contacts.html')
