bc832ad74ed27285c683564c59affca3f38b261f
User <annamarugina123@gmail.com>
2025-12-05 14:29:41 +0300

    Django_project

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def example_view(request):
+    return render(request, 'index.html')
+
+def show_data(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+
+def submit_data(request):
+    request.GET
+    request.POST
+    if request.method == 'POST':
+        return HttpResponse("Данные отправлены")
+
+def show_item(request, item_id):
+    return render(request, 'app/item.html', {'item_id': item_id})
