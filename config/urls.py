beda1f47c1ee340d48697dfce692bd0ef5a2b92a
User <annamarugina123@gmail.com>
2025-12-05 15:17:15 +0300

    Django_project

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('', include('home.urls', namespace='home')),
 ]
